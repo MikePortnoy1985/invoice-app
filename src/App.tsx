3ec7debea56cb7554b51a5912b5328ca25327f4e
Mikhail Temartsev <m.temartsev@gmail.com>
2022-06-21 23:45:08 +0300

    add tailwind config abd base variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { FC } from 'react';
 
-const App: FC = () => <div className="font-spartan">Hello from React</div>;
-
+const App: FC = () => (
+  <div className="app">
+    <h1>Test</h1>
+    <h2>Test</h2>
+    <h3>Test</h3>
+    <h3 className="text-base-bold">text-base-bold</h3>
+    <div className="text-base-medium">text-base-medium</div>
+    <div className="text-sm-custom">text-sm-custom</div>
+  </div>
+);
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,64 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', '*.{html,js}'],
   darkMode: 'class',
   theme: {
-    extend: {
-      fontFamily: {
-        spartan: ['League Spartan', 'sans-serif'],
-      },
+    fontFamily: {
+      spartan: ['League Spartan', 'sans-serif'],
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'main-light': '#F8F8FB',
+      'main-dark': '#0C0E16',
+      'custom-purple': '#7C5DFA',
+      'custom-lilac': '#9277FF',
+      'custom-red': '#EC5757',
+      'custom-pink': '#FF9797',
+      'custom-dark-1': '#141624',
+      'custom-dark-2': '#1E2139',
+      'custom-dark-gray': '#252945',
+      'custom-lavender': '#DFE3FA',
+      'custom-gray': '#888EB0',
+      'custom-light-purple': '#7E88C3',
+    },
+    fontSize: {
+      '2xl': [
+        '2rem',
+        {
+          lineHeight: '2.25rem',
+          letterSpacing: '-1px',
+        },
+      ],
+      xl: [
+        '1.25rem',
+        {
+          lineHeight: '1.375rem',
+          letterSpacing: '-0.63px',
+        },
+      ],
+      l: [
+        '1rem',
+        {
+          lineHeight: '1.5rem',
+          letterSpacing: '-0.8px',
+        },
+      ],
+      base: [
+        '0.75rem',
+        {
+          lineHeight: '0.9375rem',
+          letterSpacing: '-0.25px',
+        },
+      ],
+      sm: [
+        '0.6875rem',
+        {
+          lineHeight: '1.125rem',
+          letterSpacing: '-0.23px',
+        },
+      ],
+    },
+    extend: {},
   },
   plugins: [],
 };
